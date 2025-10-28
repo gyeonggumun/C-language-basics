622417276974f32cfcc6656f2126f05f71b77dbc
gyeonggumun <lion989072@gmail.com>
2025-10-28 22:59:19 +0900

    C 변수

--- ch1/variable.c
+++ ch1/variable.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 void main() {
 	int i = 10;
 	long I = 10L;
@@ -8,11 +9,5 @@ void main() {
 	char* str = "ABC";
 
 	//sizeof(자료형) : 소괄호 안에 작성한 변수 또는 자료형의 크기를 알려주는 함수이다.
-	printf("%d\n", sizeof(int));
-	printf("%d\n", sizeof(long));
-	printf("%d\n", sizeof(float));
-	printf("%d\n", sizeof(double));
-	printf("%d\n", sizeof(char));
-	printf("%d\n", sizeof(str));
-
+	printf("%d", sizeof(int));
 	}
---
+++ ch1/variable2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main() {
+	int age = 10;
+	float height = 125.3F;
+	double weight = 35.9;
+	char grade = 'A';
+	char* name = "문경구";
+
+	printf("나이 : age살");
+}
